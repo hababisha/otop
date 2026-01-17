066cb51a80daa66be40410f5358e5130246944f3
hababisha <haileamlakderbie1@gmail.com>
2026-01-17 23:59:48 +0300

    feat: generate 6 and 4 digit otps

--- controller/otp_controllers.go
+++ controller/otp_controllers.go
@@ -1,12 +1,20 @@
 package controller
 
-import "github.com/hababisha/otop/models"
+import (
+	"math/rand"
+    "fmt"
+	"github.com/hababisha/otop/models"
+)
 
 var store = make(map[string]models.Otp)
 
-func generateSixDigitOTP(){}
+func GenerateSixDigitOTP() string{
+    return fmt.Sprintf("%06d", rand.Intn(1000000))
+}
 	
-func generateFourDigitOTP(){}
+func GenerateFourDigitOTP() string{
+    return fmt.Sprintf("%04d", rand.Intn(10000))
+}
 
 func GenerateOTP(){} //add timestamp to the otp generated and send with theoption provided
 
