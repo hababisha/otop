c1c15304cc7d23e1eebb727005e2f1cdc40ba52d
hababisha <haileamlakderbie1@gmail.com>
2026-01-18 14:34:27 +0300

    feat: add pg driver and db connection

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-
-import "github.com/hababisha/otop/router"
+import (
+	"github.com/hababisha/otop/router"
+	"github.com/hababisha/otop/utils"
+)
 
 
 func main(){
+	utils.ConnectDB()
 	r:= router.Router()
 	r.Run(":8080")
 }
--- utils/db.go
+++ utils/db.go
@@ -1,5 +1,37 @@
 package utils
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 var DB *sql.DB
+
+func ConnectDB() {
+	if err := godotenv.Load(); err != nil{
+		log.Fatal("error loading .env")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connstr := fmt.Sprintf("sslmode=disable host=%s port=%s user=%s password=%s dbname=%s", host, port, username, password, dbName)
+	var err error
+	DB, err = sql.Open("postgres", connstr)
+	if err != nil {
+		log.Fatal("failed to open db: ", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal("failed to connect to db: ", err)
+	}
+
+}
